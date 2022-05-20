1f6ecd3adc4edbd7281d84be1a11d4eb1834a64e
dirgantoro.developer@gmail.com <dirgantoro.developer@gmail.com>
2022-05-20 14:31:55 +0700

    Update algo

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -5,45 +5,43 @@ if defined?(Rails::Console)|| Rails.env.test? || File.split($PROGRAM_NAME).last 
 end
 
 scheduler = Rufus::Scheduler.new
-scheduler.every '20s' do
+scheduler.every '1m' do
   t = Time.zone.now
   hrs = t.strftime("%I:%M")
   days = t.strftime("%A")
 
-  sch = Scheduler.first
-  listday = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
-  ll = []
-  if (sch.sun == true)
-    ll.push("Sunday")
-  end
-  if (sch.mon == true)
-    ll.push("Monday")
-  end
-  if (sch.tue == true)
-    ll.push("Tuesday")
-  end
-  if (sch.wed == true)
-    ll.push("Wednesday")
-  end
-  if (sch.thu == true)
-    ll.push("Thursday")
-  end
-  if (sch.fri == true)
-    ll.push("Friday")
-  end
-  if (sch.sat == true)
-    ll.push("Saturday")
-  end
-
-  if (ll.include?days)
-    Article.where(blast:false).each do |article|
-      author = Author.find_by_email(article.author)
-      Subscription.where(author_id:author.id).each do |subscription|
-        subscriber = Subscriber.find(subscription.subscriber_id)
-        ::TesMailer.delay(:queue => "#{subscriber.email} - #{author.name} - #{article.title}").send_email_subscriber(subscriber, author, article)
+  Scheduler.where(clock:hrs).each do |sch|
+    tmp = []
+    if(sch.sun)
+      tmp.push("Sunday")
+    end
+    if(sch.mon)
+      tmp.push("Monday")
+    end
+    if(sch.tue)
+      tmp.push("Tuesday")
+    end
+    if(sch.wed)
+      tmp.push("Wednesday")
+    end
+    if(sch.thu)
+      tmp.push("Thursday")
+    end
+    if(sch.fri)
+      tmp.push("Friday")
+    end
+    if(sch.sat)
+      tmp.push("Saturday")
+    end
+    if (tmp.include?days)
+      Article.where(blast:false).each do |article|
+        author = Author.find_by_email(article.author)
+        Subscription.where(author_id:author.id).each do |subscription|
+          subscriber = Subscriber.find(subscription.subscriber_id)
+          ::TesMailer.delay(:queue => "#{subscriber.email} - #{author.name} - #{article.title}").send_email_subscriber(subscriber, author, article)
+        end
+        article.update(blast:true)
       end
-      article.update(blast:true)
     end
   end
-
 end
