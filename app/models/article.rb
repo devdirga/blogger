a4b6602c6a04b8d3328a012d7f9a2ba1a75120df
dirgantoro.developer@gmail.com <dirgantoro.developer@gmail.com>
2022-05-27 15:42:58 +0700

    Add Tag callback

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
   belongs_to :author
+  validates :body, presence: true, length: {minimum: 10}
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,8 @@
 class Tag < ApplicationRecord
+  before_create do
+    self.title = title.tr_s(" ","-")
+  end
+  before_update do
+    self.title = title.tr_s(" ","-")
+  end
 end
